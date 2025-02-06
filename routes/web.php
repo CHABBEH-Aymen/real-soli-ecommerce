21617f0ac6b578cb06c8e449398c2cc76bcceb8c
Ayoub <elbouzidiayoub.solicode@gmail.com>
2025-02-06 20:32:09 +0100

    crud de produit sans vue et sans api

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,16 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Auth::routes();
-Route::middleware('api')->group(function(){
-    Route::apiResource('produits',ProduitController::class);
-});
+// routes/web.php
+
+//produit
+Route::get('/produits', [ProduitController::class, 'index'])->name('produits.index');
+Route::get('/produits/create', [ProduitController::class, 'create'])->name('produits.create');
+Route::post('/produits', [ProduitController::class, 'store'])->name('produits.store');
+Route::get('/produits/{id}/edit', [ProduitController::class, 'edit'])->name('produits.edit');
+Route::put('/produits/{id}', [ProduitController::class, 'update'])->name('produits.update');
+Route::delete('/produits/{id}', [ProduitController::class, 'destroy'])->name('produits.destroy');
+
 Route::get('/{vue_capture}', function () {
     $data = ['user'=>Auth::user()];
    if(Auth::check()) return view('welcome', compact('data'));
