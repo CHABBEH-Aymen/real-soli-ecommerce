6ede064275bdaa786b7af18f77e9f514c0b40cdd
ChabbehAymen <devaymen15.5.3@gmail.com>
2025-02-07 00:13:01 +0100

    fixed erro

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,12 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 
 Route::get('/{vue_capture}', function () {
-    $data = ['user'=>Auth::user()];
-   if(Auth::check()) return view('welcome', compact('data'));
+    if(Auth::check()){ 
+       $data = ['user'=>Auth::user()];
+       return view('welcome', compact('data'));
+}
    else view('welcome');
+return view('welcome');
 })->where('vue_capture', "[\/\w\.-]*");
 
 
